a8d011577ef4c2c0e8986ba4dcc76446243e9802
NightStalker <janmann500@gmail.com>
2022-08-01 09:42:26 +0300

    2022-08-01. Изменения для норм. сборки проекта.

--- app/src/main/java/com/example/theatre/features/poster/presentation/ui/PostersViewModel.kt
+++ app/src/main/java/com/example/theatre/features/poster/presentation/ui/PostersViewModel.kt
@@ -1,11 +1,7 @@
 package com.example.theatre.features.poster.presentation.ui
 
-import androidx.lifecycle.*
-import com.example.theatre.features.spectacles.domain.model.Performance
+import androidx.lifecycle.ViewModel
 import com.example.theatre.features.spectacles.domain.usecases.GetPerformanceUseCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
  * Вью-модель для фичи афиши
@@ -15,12 +11,12 @@ import kotlinx.coroutines.withContext
 class PostersViewModel(
     private val getPerformanceUseCase: GetPerformanceUseCase
 ) : ViewModel() {
-    private val _posterLoaded = MediatorLiveData<List<Performance>>()
-    val posterLoaded: LiveData<List<Performance>> get() = _posterLoaded
-
-    suspend fun init() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _posterLoaded.value = getPerformanceUseCase.getPerformance()
-        }
-    }
+//    private val _posterLoaded = MediatorLiveData<List<Performance>>()
+//    val posterLoaded: LiveData<List<Performance>> get() = _posterLoaded
+//
+//    suspend fun init() {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            _posterLoaded.value = getPerformanceUseCase.getPerformance()
+//        }
+//    }
 }

70d23acc5d91b9ef903702b87a269128427fa21a
N1ghtStalk3r <janmann500@gmail.com>
2023-01-26 11:26:49 +0300

    2023-01-26. Исправления запуска
    1. Исправлены ресурсы
    2. Пока что убран код с PosterFragment.kt

--- app/src/main/java/com/example/theatre/features/poster/presentation/ui/list/PosterFragment.kt
+++ app/src/main/java/com/example/theatre/features/poster/presentation/ui/list/PosterFragment.kt
@@ -1,19 +1,14 @@
 package com.example.theatre.features.poster.presentation.ui.list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.theatre.R
 import com.example.theatre.databinding.FragmentPosterBinding
-import com.example.theatre.features.poster.domain.model.PosterBriefItem
-import com.example.theatre.features.poster.domain.model.PosterDetails
 import com.example.theatre.features.poster.presentation.adapters.PosterBriefItemAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -38,21 +33,6 @@ class PosterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.postersList.layoutManager =
-            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-
-        adapter = PosterBriefItemAdapter {
-            id: Int -> onItemClicked(id)
-        }
-
-        binding.postersList.adapter = adapter
-
-        binding.postersList.animation =
-            AnimationUtils.loadAnimation(binding.postersList.context, R.anim.amination_poster)
-
-        initObservers()
-
         viewModel.getPosters()
     }
 
@@ -60,15 +40,6 @@ class PosterFragment : Fragment() {
         val bundle = bundleOf("poster_id" to id)
         findNavController()
             .navigate(R.id.action_home_to_posterDetailFragment, bundle)
-    }
-
-    private fun initObservers() {
-        viewModel.postersBrief.observe(viewLifecycleOwner, ::setData)
-    }
-
-
-    private fun setData(list: List<PosterBriefItem>) {
-        adapter.setData(list)
     }
 
 }
